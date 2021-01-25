91169cb69881283a4f1309b903ddbe25d43f7b1f
Richard Ingwe <richardingwe5@gmail.com>
2021-01-25 12:04:46 +0100

    added progress bar and fixed firebase import bug

---
+++ src/comps/ProgressBar.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react';
+import useStorage from '../hooks/useStorage';
+
+
+const ProgressBar = ({ setFile, file }) => {
+    const { url, progress } = useStorage(file);
+    console.log(progress, url);
+    useEffect(() => {
+        url && setFile(null);
+    }, [url, setFile]);
+    return (
+        <div className="progress-bar" style={{ width: progress + "%" }} />
+    );
+};
+
+export default ProgressBar;
