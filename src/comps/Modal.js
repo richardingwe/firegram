c5076dab2b1fcacae3468b703e29f8f47031ac66
Richard Ingwe <richardingwe5@gmail.com>
2021-01-25 21:10:39 +0100

    added framer motion to modal component

--- src/comps/Modal.js
+++ src/comps/Modal.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { motion } from "framer-motion";
 
 const Modal = ({ selectedImage, setSelectedImage }) => {
     const handleClick = (e) => {
         e.target.classList.contains("backdrop") && setSelectedImage(null);
     };
     return (
-        <div className="backdrop" onClick={handleClick}>
-            <img src={selectedImage} alt="enlarged pic" />
-        </div>
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            className="backdrop"
+            onClick={handleClick}>
+            <motion.img
+                initial={{ y: "-100vh" }}
+                animate={{ y: 0 }}
+                src={selectedImage}
+                alt="enlarged pic" />
+        </motion.div>
     );
 };
 
